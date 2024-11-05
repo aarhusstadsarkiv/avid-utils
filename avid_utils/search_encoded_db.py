a4fc82da7128545a8a134438049fa602936eb794
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-05 10:09:53 +0100

    search_encoded_db - format

--- avid_utils/search_encoded_db.py
+++ avid_utils/search_encoded_db.py
@@ -16,12 +16,12 @@ from typing import BinaryIO
 from typing import Generator
 from typing import Optional
 
-from click import Choice
-from click import IntRange
-from click import Path as ClickPath
 from click import argument
+from click import Choice
 from click import group
+from click import IntRange
 from click import option
+from click import Path as ClickPath
 from orjson import dumps
 from orjson import loads
 
@@ -85,8 +85,10 @@ class Header:
             for t in data["tables"]
         ]
 
-        return Header(hash_algorithm=data["hash_algorithm"], preserve_types=data["preserve_types"],
-                      tables=tables, bytes_data=bytes_data)
+        return Header(
+            hash_algorithm=data["hash_algorithm"], preserve_types=data["preserve_types"],
+            tables=tables, bytes_data=bytes_data
+        )
 
     def to_bytes(self):
         return pack("<L", self.length) + self.bytes
@@ -112,8 +114,9 @@ def encode_table_column(value: Any, type_byte: bytes, hash_algorithm: str, prese
 
 
 # noinspection SqlNoDataSourceInspection,SqlResolve
-def encode_table_rows(conn: Connection, table: str, hash_algorithm: str, preserve_types: bool, sample: Optional[int]
-                      ) -> Generator[bytes, None, None]:
+def encode_table_rows(
+        conn: Connection, table: str, hash_algorithm: str, preserve_types: bool, sample: Optional[int]
+) -> Generator[bytes, None, None]:
     columns: list[ColInfo] = get_columns(conn, table)
     sql: str = f'select * from "{table}" limit "{sample}"' if sample else f'select * from "{table}"'
 
@@ -132,11 +135,13 @@ def encode_database(conn: Connection, file: Path, hash_algorithm: str, preserve_
 
     for i, table in enumerate(tables):
         print(f"Getting header for table {i + 1} '{table}' ...", end=" ", flush=True)
-        header.tables.append(TableInfo(
-            name=table,
-            rows=count_rows(conn, table, sample),
-            columns=get_columns(conn, table)
-        ))
+        header.tables.append(
+            TableInfo(
+                name=table,
+                rows=count_rows(conn, table, sample),
+                columns=get_columns(conn, table)
+            )
+        )
         print("Done")
 
     with file.open("wb") as fh:
@@ -150,8 +155,10 @@ def encode_database(conn: Connection, file: Path, hash_algorithm: str, preserve_
             for j, row in enumerate(rows):
                 fh.write(row)
                 if (j % 1000) == 0:
-                    print(f"\rWriting table {i + 1} rows '{table_info.name}' ... {j / table_info.rows:02.01f}%",
-                          end=" ", flush=True)
+                    print(
+                        f"\rWriting table {i + 1} rows '{table_info.name}' ... {j / table_info.rows:02.01f}%",
+                        end=" ", flush=True
+                    )
             print(f"\rWriting table {i + 1} rows '{table_info.name}' ... Done  ")
 
 
@@ -173,8 +180,10 @@ class Database:
         return self.read(size)
 
     def encode_value(self, value_type: str, value: Any) -> bytes:
-        return encode_table_column(value, bytes([sql_types_int[value_type]]), self.header.hash_algorithm,
-                                   self.header.preserve_types)
+        return encode_table_column(
+            value, bytes([sql_types_int[value_type]]), self.header.hash_algorithm,
+            self.header.preserve_types
+        )
 
     @cached_property
     def null_hash(self) -> bytes:
@@ -210,12 +219,16 @@ class Database:
 
 def print_all_results(results: list[tuple[TableInfo, list[int]]]):
     for table, blocks in results:
-        print(f"Found match in '{table.name}'", f"{(blocks[0] // len(table.columns)) + 1}:"
-                                                f"{','.join(str((b % len(table.columns)) + 1) for b in blocks)}",
-              ' '.join(f"'{table.columns[b % len(table.columns)].name}'" for b in blocks))
+        print(
+            f"Found match in '{table.name}'", f"{(blocks[0] // len(table.columns)) + 1}:"
+                                              f"{','.join(str((b % len(table.columns)) + 1) for b in blocks)}",
+            ' '.join(f"'{table.columns[b % len(table.columns)].name}'" for b in blocks)
+        )
 
-    print(f"{len(results)} matches found",
-          f"across {len(set(t.name for [t, _] in results))} tables." if results else "")
+    print(
+        f"{len(results)} matches found",
+        f"across {len(set(t.name for [t, _] in results))} tables." if results else ""
+    )
 
 
 def print_aggregated_results(results: list[tuple[TableInfo, list[int]]]):
@@ -223,8 +236,12 @@ def print_aggregated_results(results: list[tuple[TableInfo, list[int]]]):
     tables_results: dict[(str, str), int] = {}
 
     for table, blocks in results:
-        columns: tuple[str] = tuple(sorted((table.columns[block % len(table.columns)].name for block in blocks),
-                                           key=[t.name for t in table.columns].index))
+        columns: tuple[str, ...] = tuple(
+            sorted(
+                (table.columns[block % len(table.columns)].name for block in blocks),
+                key=[c.name for c in table.columns].index
+            )
+        )
         tables[table.name] = table
         tables_results[(table.name, columns)] = tables_results.get((table.name, columns), 0) + 1
 
@@ -307,8 +324,10 @@ def find_value_parent(
     for table in db.header.tables:
         if table.name in exclude:
             continue
-        table_output = find_value_in_region(db.file, value_hash, table, db.table_offset_start(table.name),
-                                            db.table_offset_end(table.name), max_results)
+        table_output = find_value_in_region(
+            db.file, value_hash, table, db.table_offset_start(table.name),
+            db.table_offset_end(table.name), max_results
+        )
         if table_output:
             output.extend(((table, [b]) for b in table_output))
 
@@ -324,8 +343,10 @@ def find_values_parent(
     for table in db.header.tables:
         if table.name in exclude:
             continue
-        table_output = find_values_in_region(db.file, value_hashes, table, db.table_offset_start(table.name),
-                                             db.table_offset_end(table.name), max_results)
+        table_output = find_values_in_region(
+            db.file, value_hashes, table, db.table_offset_start(table.name),
+            db.table_offset_end(table.name), max_results
+        )
         output.extend((table, xs) for xs in table_output if xs)
 
     return sort_results(output)
@@ -464,12 +485,18 @@ def main():
 # noinspection GrazieInspection
 @main.command("encode", short_help="Encode a database.")
 @argument("file", required=True, type=ClickPath(exists=True, dir_okay=False, resolve_path=True, path_type=Path))
-@argument("output", required=False, default=None,
-          type=ClickPath(exists=False, dir_okay=False, resolve_path=True, path_type=Path))
-@option("--hash", "hash_algo", metavar="NAME", type=Choice(sorted(algorithms_available)), default="md5",
-        show_default=True, help="The hash algorithm to use.")
-@option("--sample", metavar="ROWS", type=IntRange(1), default=None,
-        help="Encode a random sample of ROWS rows for each table.")
+@argument(
+    "output", required=False, default=None,
+    type=ClickPath(exists=False, dir_okay=False, resolve_path=True, path_type=Path)
+)
+@option(
+    "--hash", "hash_algo", metavar="NAME", type=Choice(sorted(algorithms_available)), default="md5",
+    show_default=True, help="The hash algorithm to use."
+)
+@option(
+    "--sample", metavar="ROWS", type=IntRange(1), default=None,
+    help="Encode a random sample of ROWS rows for each table."
+)
 @option("--ignore-types", is_flag=True, default=False, help="Do not encode type information.")
 @timer("Converted database in")
 def encode(file: Path, output: Path, hash_algo: str, ignore_types: bool, sample: Optional[int]):
@@ -487,18 +514,26 @@ def encode(file: Path, output: Path, hash_algo: str, ignore_types: bool, sample:
 
 @main.command("search", short_help="Search an encoded database.")
 @argument("file", required=1, type=ClickPath(exists=True, dir_okay=False, resolve_path=True, path_type=Path))
-@option("--value", metavar="<SQL-TYPE JSON-VALUE>...", type=(Choice(list(sql_types_int.keys())), str),
-        multiple=True, help="Search for specific values.")
-@option("--cell", metavar="<TABLE ROW COLUMN>", type=(str, IntRange(1), IntRange(1)),
-        help="Search for the value in a cell.")
-@option("--column", metavar="<TABLE COLUMN>", type=(str, IntRange(1)),
-        help="Search for all values in column.")
+@option(
+    "--value", metavar="<SQL-TYPE JSON-VALUE>...", type=(Choice(list(sql_types_int.keys())), str),
+    multiple=True, help="Search for specific values."
+)
+@option(
+    "--cell", metavar="<TABLE ROW COLUMN>", type=(str, IntRange(1), IntRange(1)),
+    help="Search for the value in a cell."
+)
+@option(
+    "--column", metavar="<TABLE COLUMN>", type=(str, IntRange(1)),
+    help="Search for all values in column."
+)
 @option("--max-results", metavar="INTEGER", type=IntRange(1), help="Stop after INTEGER results.")
 @option("--include-null", is_flag=True, default=False, help="Do not skip null values.")
 @option("--show-all-results", is_flag=True, default=False, help="Do not aggregate results.")
 @timer("Search completed in")
-def find(file: Path, value: tuple[tuple[str, str]], cell: Optional[tuple[str, int, int]],
-         column: Optional[tuple[str, int]], max_results: Optional[int], include_null: bool, show_all_results: bool):
+def find(
+        file: Path, value: tuple[tuple[str, str]], cell: Optional[tuple[str, int, int]],
+        column: Optional[tuple[str, int]], max_results: Optional[int], include_null: bool, show_all_results: bool
+):
     """
     Search for specific values, cells, or columns inside an encoded FILE.
 
